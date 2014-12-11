cbc73ba6fc339a29a2ceb82a468582ea6c330eb0
Alberto <a.trillo.garcia@gmail.com>
2014-12-11 19:09:05 +0100

    Estructura de preguntas

--- index.php
+++ index.php
@@ -4,7 +4,22 @@
 
     <?php
         if (file_exists("conf-bd")){
-            // LOGIN + PAGINAS AQUI
+
+            if(isset($_SESSION["user"])){
+                if(isset($_SESSION["p1"],$_SESSION["p2"],$_SESSION["p3"])){
+                    //PAGINA FINAL
+                }
+                else if(isset($_SESSION["p1"],$_SESSION["p2"])){
+                    //PREGUNTA 3
+                }
+                else if(isset($_SESSION["p1"])){
+                    //PREGUNTA 2
+                }else {
+                    //PREGUNTA 1
+                }
+            }else{
+                include_once "registro.php";
+            }
         }else{
             include_once "createDB.php";
         }
