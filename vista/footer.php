e1a2f5ec26b84862119dc6a4a9a0e4380f5f0310
jsm94 <smjuan24@gmail.com>
2014-12-11 19:02:28 +0100

    Footer

---
+++ vista/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p class="center muted credit">Hecho con <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> por <a href="https://github.com/albertotrillo">Alberto Trillo</a>, <a href="https://github.com/nacholozano">Nacho Lozano</a> y <a href="https://github.com/jsm94">Juan Antonio Seco</a>.</p>
+    </div>
+</footer>
