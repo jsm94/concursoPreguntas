e9cb4fdec629939c021352c792de38a5cd4773cc
jsm94 <smjuan24@gmail.com>
2014-12-11 18:48:20 +0100

    Creando carpetas y el head.php

---
+++ vista/head.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <!-- CDN Bootstrap -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necesario para Bootstrap JS) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <title>Concurso de Preguntas</title>
+    <meta name="description" content="Concurso de preguntas y respuestas">
+    <meta name="viewport" content="width=device-width">
+    <!-- TODO: Aquí se incluirán los modelos necesarios -->
+</head>
