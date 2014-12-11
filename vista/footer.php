71670e0ce43f4a30a5eb46b88c3a56129b044ad9
jsm94 <smjuan24@gmail.com>
2014-12-11 19:22:19 +0100

    Cambios en el footer

--- vista/footer.php
+++ vista/footer.php
@@ -1,5 +1,7 @@
-<footer>
+<footer class="mastfoot">
     <div class="container">
-        <p class="center muted credit">Hecho con <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> por <a href="https://github.com/albertotrillo">Alberto Trillo</a>, <a href="https://github.com/nacholozano">Nacho Lozano</a> y <a href="https://github.com/jsm94">Juan Antonio Seco</a>.</p>
+       <div class="col-md-6 col-md-offset-3">
+           <p class="muted credit">Hecho con <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> por <a href="https://github.com/albertotrillo">Alberto Trillo</a>, <a href="https://github.com/nacholozano">Nacho Lozano</a> y <a href="https://github.com/jsm94">Juan Antonio Seco</a>.</p>
+       </div>
     </div>
 </footer>
